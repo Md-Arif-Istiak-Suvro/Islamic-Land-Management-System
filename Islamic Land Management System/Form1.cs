2e5b54424ceecc1dddc5d310d870bbad0095a3e1
Arif-istiak <suvro.ss06@gmail.com>
2026-04-29 19:37:21 +0600

    Demo Modified

--- Islamic Land Management System/Form1.cs
+++ Islamic Land Management System/Form1.cs
@@ -17,12 +17,11 @@ namespace Islamic_Land_Management_System
         {
             InitializeComponent();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             Close();
         }
-
+        
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
